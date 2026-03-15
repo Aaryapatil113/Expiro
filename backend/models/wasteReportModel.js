d6b95b2634f4cf6b32d34400dcc439f0b86c77cf
Kanad Motiwale <159768250+kanadmotiwale@users.noreply.github.com>
2026-03-15 19:05:35 -0400

    Add wasteReportModel with CRUD functions
    Implement CRUD operations for waste reports in the model.

---
+++ backend/models/wasteReportModel.js
@@ -0,0 +1,39 @@
+import { ObjectId } from 'mongodb';
+import { getDB } from '../config/db.js';
+
+const COLLECTION = 'waste_reports';
+
+const findAll = async () => {
+  return await getDB().collection(COLLECTION).find().toArray();
+};
+
+const findById = async (id) => {
+  return await getDB()
+    .collection(COLLECTION)
+    .findOne({ _id: new ObjectId(id) });
+};
+
+const findByProduct = async (productId) => {
+  return await getDB()
+    .collection(COLLECTION)
+    .find({ productId: new ObjectId(productId) })
+    .toArray();
+};
+
+const insertOne = async (report) => {
+  return await getDB().collection(COLLECTION).insertOne(report);
+};
+
+const updateOne = async (id, updates) => {
+  return await getDB()
+    .collection(COLLECTION)
+    .updateOne({ _id: new ObjectId(id) }, { $set: updates });
+};
+
+const deleteOne = async (id) => {
+  return await getDB()
+    .collection(COLLECTION)
+    .deleteOne({ _id: new ObjectId(id) });
+};
+
+export default { findAll, findById, findByProduct, insertOne, updateOne, deleteOne };
